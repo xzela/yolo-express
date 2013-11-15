d993c0e2685aaad443f46c42b66fe6760fcb3b8b
Zeph Lafassett <zeph@lafassett.com>
2013-11-15 13:51:33 -0800

    adding blog entries

--- blog.js
+++ blog.js
@@ -5,6 +5,7 @@ var entries = [
 {"id":4, "title":"Mean People Suck", "body":"People who are mean aren't nice or fun to hang around.", "published":"6/5/2013"},
 {"id":5, "title":"I'm Leaving Technology X and You Care", "body":"Let me write some link bait about why I'm not using a particular technology anymore.", "published":"6/10/2013"},
 {"id":6, "title":"Help My Kickstarter", "body":"I want a new XBox One. Please fund my Kickstarter.", "published":"6/12/2013"}];
+
 exports.getBlogEntries = function() {
    return entries;
 }
--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var app = express();
 // includes the handlebars
 var hbs = require('hbs');
 
+// load the blog data/engine (at this point they're the same)
 var blogEngine = require('./blog');
 
 // set the engine to look for .html files in addition to .hbs
@@ -16,16 +17,21 @@ app.set('view engine', 'html');
 // load the handlebars
 app.engine('html', hbs.__express);
 
+//app.use(express.bodyParser());
+app.use(express.static('public'));
+
+
 app.get('/', function(req, res) {
-  res.render('index', {title: 'Yolo! Express'});
+  res.render('index', {title: 'Yolo! Express', entries: blogEngine.getBlogEntries});
 });
 
 app.get('/about', function(req, res) {
-  res.render('index', {title: 'Aboutn Yolo! Express'});
+  res.render('about', {title: 'Aboutn Yolo! Express'});
 });
 
-app.get('/article', function(req, res) {
-  res.render('about');
+app.get('/article/:id', function(req, res) {
+	var entry = blogEngine.getBlogEntry(req.params.id);
+  res.render('article', {title:entry.title, blog:entry});
 });
 
 console.log("Listening on port: " + PORT);
